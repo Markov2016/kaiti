853aa0cebbf4e2f9f48c11fbf9d2356b68ffebf8
Markov2016 <yang-yx13@outlook.com>
2017-01-10 15:14:55 +0800

    updata of dds_sram.v

--- verilog_code/dds_sram/scripts/dds_sram.v
+++ verilog_code/dds_sram/scripts/dds_sram.v
@@ -1,13 +1,18 @@
-module dds_sram(sys_clk,reset,fcw,offset,writed,sin_amp);
+module dds_sram(sys_clk,reset,fcw,offset,writed_,sin_amp);
   input sys_clk,reset;
   input [31:0] fcw,offset;
   output [15:0] sin_amp;
-  output writed;
+  output writed_;
   
+  wire writed;
   wire [13:0] index;
   wire [15:0] phase;
-  
-  PA32 pa(sys_clk,reset,offset,fcw,phase);
+  wire reset_,cen;
+  assign cen = 0;
+  assign reset_ = reset && writed;
+  assign writed_ = reset_;
+
+  PA32 pa(sys_clk,reset_,offset,fcw,phase);
   phase_compression_16 pc(phase,index);
-  PAC pac(sys_clk,reset,phase[15],index,writed,sin_amp);
+  PAC pac(sys_clk,reset,cen,phase[15],index,writed,sin_amp);
 endmodule
